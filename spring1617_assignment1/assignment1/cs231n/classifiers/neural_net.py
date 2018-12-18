fixed minor bug in softmax loss

spring1617_assignment1/assignment1/cs231n/classifiers/neural_net.py
@@ -97,8 +97,8 @@ class TwoLayerNet(object):
     # classifier loss.                                                          #
     #############################################################################
     nList = range(N)
-    data_loss = -scores[nList, y]
-    data_loss = 0.0 
+    # data_loss = -scores[nList, y]
+    # data_loss = 0.0 
     scores -= np.max(scores) # ensure stability
     scores_exp = np.exp(scores)   # ==> e ^ scores
     denims = np.sum(scores_exp, axis=1) # denimenator = sum of e^score

spring1617_assignment1/assignment1/cs231n/classifiers/softmax.py
@@ -68,16 +68,17 @@ def softmax_loss_vectorized(W, X, y, reg):
   loss = 0.0
   dW = np.zeros_like(W)
 
-  num_train = X.shape[0]
+  N = num_train = X.shape[0]
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  f = X.dot(W) # all scores (N x C)
+  f = X.dot(W) # all scores (N x C)  
+  f -= np.max(f, axis=1, keepdims=True)
   f_exp = np.exp(f)
-  loss += (-1 * np.sum(f[range(X.shape[0]), y])) + np.log(np.sum(f_exp))
+  loss += np.sum((-1 * (f[range(X.shape[0]), y])) + np.log(np.sum(f_exp, axis=1)))
 
   loss /= num_train
   loss += 0.5 * reg * np.sum(W * W)